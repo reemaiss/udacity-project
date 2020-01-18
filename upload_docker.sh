1883b086da1d60f72de1553219df4a49c3150403
reema al-swailam <32641581+reemaiss@users.noreply.github.com>
2020-01-18 10:20:21 +0000

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -10,6 +10,8 @@ dockerpath=2030rema/api
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login localhost:8080
+docker tag 6a8f19eaf1f0 2030rema/httpd:version1.0
 
 # Step 3:
 # Push image to a docker repository
